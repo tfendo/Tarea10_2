9cc059ec8698ff5eef5401ec738d47be1ff19045
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-18 16:08:26 +0200

    Task_6

---
+++ app/src/main/java/com/hugoguillin/roomwordssample/WordRepository.java
@@ -0,0 +1,38 @@
+package com.hugoguillin.roomwordssample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+
+    private WordDao palabra;
+    private LiveData<List<Word>> allWords;
+
+    WordRepository(Application app){
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(app);
+        palabra = db.wordDao();
+        allWords = palabra.getAllWords();
+    }
+
+    public void insert(Word word){
+        new insertAsyncTask(palabra).execute(word);
+    }
+
+    private static class insertAsyncTask extends AsyncTask<Word, Void, Void>{
+
+        private WordDao asyncTaskDao;
+
+        insertAsyncTask(WordDao dao){
+            asyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(Word... words) {
+            asyncTaskDao.insert(words[0]);
+            return null;
+        }
+    }
+}
