3afe5b9183525923b051c9eec2da76800fc20f2b
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-18 13:32:56 +0200

    Task_2

---
+++ app/src/main/java/com/hugoguillin/roomwordssample/Word.java
@@ -0,0 +1,22 @@
+package com.hugoguillin.roomwordssample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "tabla_palabras")
+public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "Palabra")
+    private String palabra;
+
+    public Word(@NonNull String s){
+        palabra = s;
+    }
+
+    public String getPalabra(){
+        return palabra;
+    }
+}
