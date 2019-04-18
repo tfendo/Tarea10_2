e9164a583d772c816beb7c66ea53012b9ef987e5
Hugo Guillín <hugoguillin@hotmail.com>
2019-04-18 17:18:41 +0200

    Task_10

--- app/src/main/java/com/hugoguillin/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/hugoguillin/roomwordssample/WordRoomDatabase.java
@@ -1,15 +1,26 @@
 package com.hugoguillin.roomwordssample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
 
     public abstract WordDao wordDao();
     private static WordRoomDatabase INSTANCE;
+
+    private static RoomDatabase.Callback callback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+
+        }
+    };
 
     public static WordRoomDatabase getDatabase(final Context context){
         if(INSTANCE == null){
@@ -18,10 +29,33 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(callback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+
+        private final WordDao dao;
+        String[] palabras = {"delfin", "cocodrilo", "cobra"};
+
+        PopulateDbAsync(WordRoomDatabase db){
+            dao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            dao.deleteAll();
+
+            for (int i = 0; i <= palabras.length-1; i++) {
+                Word palabra = new Word(palabras[i]);
+                dao.insert(palabra);
+            }
+            return null;
+        }
     }
 }
